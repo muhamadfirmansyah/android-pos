33184b977b7c62c909903af8fe7c57ab9bf7085b
Muhamad Firmansyah <firmansyah.assignment@gmail.com>
2021-08-29 18:36:40 +0700

    Final Project

---
+++ app/src/main/java/com/firmansyah/aplikasi_point_of_sales/hal_login.java
@@ -0,0 +1,13 @@
+package com.firmansyah.aplikasi_point_of_sales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hal_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hal_login);
+    }
+}
---
+++ app/src/main/java/com/firmansyah/aplikasi_point_of_sales/tambah_barang.java
@@ -0,0 +1,29 @@
+package com.firmansyah.aplikasi_point_of_sales;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class tambah_barang extends Fragment {
+
+
+    public tambah_barang() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tambah_barang, container, false);
+    }
+
+}
